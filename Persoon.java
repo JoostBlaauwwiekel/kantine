7d8081412959c6e3327540e235da933f7e15c7ab
Joost <keeperjoost@gmail.com>
2019-05-23 09:52:30 +0200

    added class Persoon

---
+++ Persoon.java
@@ -0,0 +1,88 @@
+/**
+ * @version 1.0
+ * @author Joost Blaauwwiekel
+ *
+ * Class Persoon
+ *
+ */
+public class Persoon {
+
+    public int BSN;
+    public String voornaam;
+    public String achternaam;
+    public Datum geboortedatum;
+    public char geslacht;
+
+    public Persoon(int BSN, String voornaam, String achternaam, char geslacht, int dag, int maand, int jaar) {
+        this.BSN = BSN;
+        this.voornaam = voornaam;
+        this.achternaam = achternaam;
+        this.geslacht = geslacht;
+        this.geboortedatum = new Datum(dag, maand, jaar);
+    }
+
+    public Persoon() {
+        BSN = 0;
+        voornaam = "";
+        achternaam = "";
+        geslacht = Character.MIN_VALUE; //empty char value
+
+    }
+
+    public void setBSN(int BSN) {
+        this.BSN = BSN;
+    }
+
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    public void setGeslacht(char geslacht) {
+
+        if(this.geslacht == 'm' || this.geslacht == 'v') {
+            this.geslacht = geslacht;
+        } else {
+            //do something else, not sure yet what should happen
+        }
+    }
+
+    public int getBSN() {
+        return BSN;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public String getGeboortedatum() {
+        return geboortedatum.getDatumAsString();
+    }
+
+    public String getGeslacht() {
+
+        if(geslacht == 'm' || geslacht == 'M') {
+            return "Man";
+        }
+        if(geslacht == 'v' || geslacht == 'V') {
+            return "Vrouw";
+        }
+
+        //else
+        return "Onbekend";
+
+    }
+
+    @override
+    public void toString() {
+        System.out.println("BSN: " + BSN + "; Naam: " + voornaam + " " + achternaam + "; Geboortedatum: " + geboortedatum + ";  Geslacht: " + geslacht);
+    }
+
+}
