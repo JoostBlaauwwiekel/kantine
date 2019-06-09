739743039691668bd3f077c12711dec49cceefa8
Hessel Jager <hessel_fj@hotmail.com>
2019-06-10 01:08:45 +0200

    Administratie afgemaakt

--- Administratie.java
+++ Administratie.java
@@ -1,12 +1,20 @@
 public class Administratie {
 
+    final static int DAYS_IN_WEEK = 7;
+
+    /**
+     * Constructor voor Administratie, deze is private zodat
+     * je niet per ongeluk een instantie van deze klasse kunt maken
+     */
+    private Administratie() { }
+
     /**
      * Deze methode berekent van de int array aantal de gemiddelde waarde
      *
      * @param aantal aantak
      * @return het gemiddelde
      */
-    public double berekenGemiddeldAantal(int[] aantal) {
+    public static double berekenGemiddeldAantal(int[] aantal) {
         double totaal = 0;
         for (double i : aantal) {
             totaal += i;
@@ -24,7 +32,7 @@ public class Administratie {
      * @param omzet omzet
      * @return het gemiddelde
      */
-    public double berekenGemiddeldeOmzet(double[] omzet) {
+    public static double berekenGemiddeldeOmzet(double[] omzet) {
         double totaal = 0;
         for (double i : omzet) {
             totaal += i;
@@ -44,15 +52,14 @@ public class Administratie {
      */
 
     public static double[] berekenDagOmzet(double[] omzet) {
-        double[] temp = new double[7];
-        for(int i = 0; i < 7; i++) {
+        double[] temp = new double[DAYS_IN_WEEK];
+        for(int i = 0; i < DAYS_IN_WEEK; i++) {
 
             int j = 0;
-            while( ... ) {
-                temp[i] += omzet[i + 7 * j];
-
+            while(i + DAYS_IN_WEEK * j < omzet.length) {
+                temp[i] += omzet[i + DAYS_IN_WEEK * j];
+                j++;
                 // omitted
-
             }
         }
         return temp;
